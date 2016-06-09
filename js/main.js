4dcc66d2386035bb27b5460ba03f88208078523b
Spethreeker <sallegrezz@gmail.com>
2016-06-09 12:27:28 -0500

    Hero and picture added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggleCom(){
+  var e = document.GetElementById(ComPanel){
+    if(e.style.display == 'block'){
+      e.style.display = 'none';
+    }else{
+      e.style.display = 'block';
+    }
+  }
+}
