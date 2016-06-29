d9d4235a1300b8a3e7d5aabd4da38c26c0e91e47
Spethreeker <sallegrezz@gmail.com>
2016-06-29 17:12:37 -0500

    About to start using Slick

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function toggleCom(){
-  var e = document.GetElementById(ComPanel){
-    if(e.style.display == 'block'){
-      e.style.display = 'none';
-    }else{
-      e.style.display = 'block';
-    }
-  }
-}
