30efa4339dbafd4866b4e9cb640c68f079e459d1
Spethreeker <sallegrezz@gmail.com>
2017-01-11 16:00:37 -0600

    Fixed grunt on windows, changed menu buttons

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,6 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		
-		
 		cssmin: {
 		my_target: {
 			files:[{
@@ -12,13 +10,10 @@ module.exports = function(grunt) {
 				src: ['*.css', '!*.min.css'],
 				dest: 'css/',
 				ext: '.min.css'
-
 			}]
-			
 		}
 	}
 });
-
 	//load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
